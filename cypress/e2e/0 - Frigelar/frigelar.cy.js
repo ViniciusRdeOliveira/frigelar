2b0409c327ed419ec2157852144bf9a0cbc7354e
Vinicius  Oliveira <vinicius.r.oliveira@outlook.com.br>
2026-07-20 12:40:45 -0300

    Desenvolvendo teste de compra

--- cypress/e2e/0 - Frigelar/frigelar.cy.js
+++ cypress/e2e/0 - Frigelar/frigelar.cy.js
@@ -1,44 +1,67 @@
 /// <reference types = "cypress" />
 
 describe('Acesso site',()=>{
- before(()=>{
+ beforeEach(()=>{
     cy.viewport(1366, 768);
     cy.visit('https://www.frigelar.com.br');
     
  })
 
- beforeEach(()=>{
-    cy.reload();
- })
-
-    it.only('Deve fazer login',()=>{
-        
-        cy.get('.login-text', {timeout: 30000})
+    it('Deve fazer login',()=>{
+        cy.get('#frigelar-login', {timeout: 10000})
+        //cy.get('.login-text', {timeout: 30000})
             .should('be.visible')
             .click()
-        cy.get('#email').type('viniciusribeiro@frigelar.com.br');
-        cy.get('#password').type('anthonyA1#');
+       cy.get('#email', { timeout: 10000 })
+            .should('be.visible')
+            .click()
+            .clear()
+            .type('viniciusribeiro@frigelar.com.br')
+    .should('have.value', 'viniciusribeiro@frigelar.com.br');
+        cy.get('[name="password"]')
+            .should('be.visible')
+            .click()
+            .clear()
+            .type('anthonyA1#');
         cy.get('#login-form > :nth-child(5) > .primaryBtn').click();
-        cy.get('.logged > b').should('contain','Vinicius');
-
+        cy.get('.logged > b', {timeout: 10000})
+            .should('contain','Vinicius');
+        
         
     })
 
-    it.only('Deve Buscar produto',()=>{
-        cy.get('#search-input', {timeout: 30000})
-            .should('be.visible')
-            .click()
-            .type('Desumidificador de Ar');
+    it('Deve Buscar produto', () => {
+
+        cy.get('#search-input')
+            .click();
+
+        cy.get('#search-input', { timeout: 10000 })
+            .should('not.have.attr', 'readonly');
+
+        cy.get('#search-input', { timeout: 10000 })
+            .type('Desumidificador de Ar', { delay: 50 });
+
         cy.get('.search-button').click();
-        cy.get('.productListingContainer', {timeout: 10000}).should('be.visible');
-    })
 
-    it.only('AddToCart', ()=>{
+        cy.get('.productListingContainer', { timeout: 10000 })
+            .should('be.visible');
+
+})
+
+it.only('AddToCart', ()=>{
         cy.visit('https://www.frigelar.com.br/desumidificador-de-ar-antimofo-permanente-eos-bivolt/p/kit1779', {timeout: 10000})
+        cy.get('.product-name')
+            .should('contain', 'Desumidificador de Ar EOS Antimofo Permanente Bivolt', { timeout: 10000 })
+        cy.get('.new-price-container > .old-price', { timeout: 10000 })
+            .should('exist').and('be.visible');
+        cy.get('.flex-wrap > :nth-child(1) > .price',{ timeout: 10000 })
+            .should('exist').and('be.visible');
+        cy.get('#buy-button')
+            .click();
+        cy.get('#warrantyModal > .modal-dialog > .modal-content',{ timeout: 10000 })
+            .should('exist').and('be.visible')
         
-        cy.server()
-
-        cy.route('POST', '**/logistic').as('logistic')
+      /* cy.intercept('POST', '/api/logistic').as('logistic');
 
        // cy.get('#btnCalcularFrete').click()
 
@@ -46,7 +69,7 @@ describe('Acesso site',()=>{
             expect(xhr.status).to.equal(200)
             expect(xhr.responseBody).to.have.property('success', true)
 
-        })
+        })*/
     })
         
 })   
